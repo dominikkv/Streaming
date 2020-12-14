3ba728d60be05afca2089ebd08e5a8cf55e1ab9c
Dominik Vorreiter <dominikkv@gmx.de>
2020-12-14 23:55:36 +0100

    Rename file

--- src/XStreaming.h
+++ src/XStreaming.h
@@ -17,8 +17,8 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef ARDUINO_STREAMING
-#define ARDUINO_STREAMING
+#ifndef XStreaming_H
+#define XStreaming_H
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
